28332970750d07e1cd5a62f737b001e16054a156
chenjinghao <chenjinghao@cvte.com>
2023-08-27 18:08:51 +0800

    [config] add rainbow

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -97,6 +97,9 @@ packer.startup({
       "nvim-treesitter/nvim-treesitter",
       commit = '4cccb6f494eb255b32a290d37c35ca12584c74d0',
     })
+
+    use("luochen1990/rainbow")
+
     -- -- dashboard-nvim
     -- use("glepnir/dashboard-nvim")
     use("junegunn/fzf")
@@ -128,21 +131,36 @@ packer.startup({
     -- use 'dnlhc/glance.nvim'
 
     --------------------- LSP --------------------
-    use({ "williamboman/nvim-lsp-installer" })
+    use("williamboman/nvim-lsp-installer")
     -- Lspconfig
-    use({ "neovim/nvim-lspconfig" })
-	use {'hrsh7th/cmp-nvim-lsp'}
-	use {'hrsh7th/cmp-buffer'}
-	use {'hrsh7th/cmp-path'}
-	use {'hrsh7th/cmp-cmdline'}
-	use {'hrsh7th/nvim-cmp'}
-	-- vsnip
-	use {'hrsh7th/cmp-vsnip'}
-	use {'hrsh7th/vim-vsnip'}
-    -- 常见编程语言代码段
-	use {'rafamadriz/friendly-snippets'}
-	-- lspkind
-	use {'onsails/lspkind-nvim'}
+    use("neovim/nvim-lspconfig")
+    use {
+        "hrsh7th/nvim-cmp",
+        requires = {
+            "hrsh7th/cmp-nvim-lsp", --neovim 内置 LSP 客户端的 nvim-cmp 源
+            --以下插件可选，可以根据个人喜好删减
+            "hrsh7th/cmp-buffer", --从buffer中智能提示
+            "hrsh7th/cmp-nvim-lua", --nvim-cmp source for neovim Lua API.
+            "hrsh7th/cmp-path", --自动提示硬盘上的文件
+            "hrsh7th/cmp-calc", --输入数学算式（如1+1=）自动计算
+            "hrsh7th/cmp-emoji", --输入: 可以显示表情
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-vsnip", -- vsnip
+            "hrsh7th/vim-vsnip", -- vsnip
+            "octaltree/cmp-look", --用于完成英语单词
+            "onsails/lspkind-nvim", --美化自动完成提示信息
+            "f3fora/cmp-spell" --nvim-cmp 的拼写源基于 vim 的拼写建议
+        }
+    }
+    -- 代码段提示
+    use {
+        "L3MON4D3/LuaSnip",
+        requires = {
+            "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+            "rafamadriz/friendly-snippets" --代码段合集
+        }
+    }
+
     -- 补全引擎
     -- Snippet 引擎
     -- 补全源
@@ -150,8 +168,8 @@ packer.startup({
 
     -- LSP UI 增强
     use{"glepnir/lspsaga.nvim"}
-
     use {'simrat39/symbols-outline.nvim'}
+
 
     -- -- UI 增强
     -- use("onsails/lspkind-nvim")
