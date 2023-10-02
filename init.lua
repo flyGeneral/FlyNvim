b872b1cb3705db6e3da8ada6cf7148fcab3b557b
chenjinghao <chenjinghao@cvte.com>
2023-10-02 14:26:29 +0800

    [config][nvim-cmp][neodev][autopair] 增加自动补全和提示插件

--- init.lua
+++ init.lua
@@ -16,6 +16,39 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
+  	{
+		event = "VeryLazy",
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+		},
+	},
+    	{
+		"windwp/nvim-autopairs",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+  {
+	  event = "VeryLazy",
+	  "williamboman/mason.nvim",
+	  build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+  },
+  {
+	event = "VeryLazy",
+	"neovim/nvim-lspconfig",
+	dependencies = { "williamboman/mason-lspconfig.nvim" },
+  },
+  	{
+		"folke/neodev.nvim",
+	},
   {
     "RRethy/nvim-base16",
     lazy = true,
@@ -31,3 +64,15 @@ require("lazy").setup({
   },
 })
 vim.cmd.colorscheme("base16-tender")
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+-- Set up lspconfig.
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("neodev").setup({
+	-- add any options here, or leave empty to use the default settings
+})
+
+require("lsp-config.cmp")
+require("lsp-config.lspconfig")
