85a5d6df4559c16ea3370f0329b217c252d1569e
chenjinghao <chenjinghao@cvte.com>
2023-10-03 10:45:45 +0800

    [config][cmp] 迁移自动补全 cmp 插件

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -45,6 +45,44 @@ require("lazy").setup({
             require("nvim-autopairs").setup({})
         end,
     },
+    -- lspconfig
+        --------------------- LSP 代码补全，自动提示 --------------------
+    {"williamboman/nvim-lsp-installer"},
+    -- Lspconfig
+    {"neovim/nvim-lspconfig"},
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp", --neovim 内置 LSP 客户端的 nvim-cmp 源
+            --以下插件可选，可以根据个人喜好删减
+            "hrsh7th/cmp-buffer", --从buffer中智能提示
+            "hrsh7th/cmp-nvim-lua", --nvim-cmp source for neovim Lua API.
+            "hrsh7th/cmp-path", --自动提示硬盘上的文件
+            "hrsh7th/cmp-calc", --输入数学算式（如1+1=）自动计算
+            "hrsh7th/cmp-emoji", --输入: 可以显示表情
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-vsnip", -- vsnip
+            "hrsh7th/vim-vsnip", -- vsnip
+            "octaltree/cmp-look", --用于完成英语单词
+            "onsails/lspkind-nvim", --美化自动完成提示信息
+            "f3fora/cmp-spell" --nvim-cmp 的拼写源基于 vim 的拼写建议
+        },
+    },
+    -- 代码段提示
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip", -- Snippets source for nvim-cmp
+            "rafamadriz/friendly-snippets" --代码段合集
+        },
+    },
+    -- 补全引擎
+    -- Snippet 引擎
+    -- 补全源
+    {"hrsh7th/cmp-nvim-lsp-signature-help" },
+    -- LSP UI 增强
+    {"glepnir/lspsaga.nvim"},
+    {'simrat39/symbols-outline.nvim'},
 })
 
 
@@ -52,8 +90,6 @@ require("lazy").setup({
 -- require("plugin-config.nvim-treesitter")
 -- nvim-tree
 require("plugin-config.nvim-tree")
--- 文件大纲
--- require("plugin-config.symbols-outline")
 -- 插件配置
 -- which key
 require("plugin-config.which-key")
@@ -63,17 +99,14 @@ require("plugin-config.fzf")
 require("plugin-config.lualine");
 -- bufferline
 require("plugin-config.bufferline")
--- nvimtree
--- require("plugin-config.nvimtree")
--- telescope 文件模糊搜索
--- require("plugin-config.telescope")
 -- lsp 配置
--- require("lsp.setup")
+require("lsp.setup")
 -- lsp auto complete
--- require('plugin-config.cmp')
+require('plugin-config.cmp')
+-- 文件大纲
+require("plugin-config.symbols-outline")
 -- theme config
 require("plugin-config.tokyonight")
--- require('plugin-config.vscode')
 -- comment
 require("plugin-config.Comment")
 
