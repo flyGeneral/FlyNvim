53057b17588f0a2dd6637142d246f7b59646a2e0
chenjinghao <chenjinghao@cvte.com>
2023-10-04 11:49:07 +0800

    [plugins][git] 增加 git 插件

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -83,6 +83,25 @@ require("lazy").setup({
     -- LSP UI 增强
     {"glepnir/lspsaga.nvim"},
     {'simrat39/symbols-outline.nvim'},
+
+    -- git plugin
+    {
+        event = "VeryLazy",
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end,
+    },
+    {
+        event = "VeryLazy",
+        "tpope/vim-fugitive",
+        cmd = "Git",
+        config = function()
+            -- convert
+            vim.cmd.cnoreabbrev([[git Git]])
+            vim.cmd.cnoreabbrev([[gp Git push]])
+        end,
+    },
 })
 
 
