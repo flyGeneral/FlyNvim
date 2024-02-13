e607643f7f8be46828111c9c571d3f67ef69a0d1
chenjinghao <chenjinghao@cvte.com>
2024-02-13 21:38:57 +0800

    [theme] review theme night

--- lua/plugin-config/tokyonight.lua
+++ lua/plugin-config/tokyonight.lua
@@ -10,8 +10,8 @@ vim.g.tokyonight_italic_comments = true
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "Night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  light_style = "Night", -- The theme is used when the background is set to light
+  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "night", -- The theme is used when the background is set to light
   transparent = false, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
   styles = {
