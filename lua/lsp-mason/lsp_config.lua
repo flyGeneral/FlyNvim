f55c0583509dd3133f7ffbdf6d04060ec8833e16
chenjinghao <chenjinghao@cvte.com>
2024-02-15 09:16:45 +0800

    [config] review lsp config

--- lua/lsp-mason/lsp_config.lua
+++ lua/lsp-mason/lsp_config.lua
@@ -68,6 +68,7 @@ cmp.setup({
 	}),
 })
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("lspconfig").lua_ls.setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
