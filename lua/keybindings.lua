953862f8dafcccaa7b9769d4cba07410fb90bda2
chenjinghao <chenjinghao@cvte.com>
2024-02-22 17:06:23 +0800

    [keybinding] review split window and move keybinding in window panel

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -31,17 +31,19 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 })
 
 -- nvim panel move
-vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
-vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
+vim.keymap.set("n", "<A-l>", "<C-w>l", opt)
+vim.keymap.set("n", "<A-h>", "<C-w>h", opt)
+vim.keymap.set("n", "<A-j>", "<C-w>j", opt)
+vim.keymap.set("n", "<A-k>", "<C-w>k", opt)
 -- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
 vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
 vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
 
 -- nvim split screen
-vim.keymap.set("n", "<Leader>v", "<C-w>v", opt)
-vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
+vim.keymap.set("n", "vv", "<C-w>v", opt)
+vim.keymap.set("n", "vs", "<C-w>s", opt)
+vim.keymap.set("n", "vc", "<C-w>c", opt)
+vim.keymap.set("n", "vo", "<C-w>o", opt)
 
 vim.api.nvim_set_keymap("n", "<leader>-", ":new <CR>", {noremap = true, silent = true, desc = "horizontal split"})
 vim.api.nvim_set_keymap("n", "<leader>=", ":vertical new <CR>", {noremap = true, silent = true, desc = "vertical split" })
