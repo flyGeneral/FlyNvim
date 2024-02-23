d1f4d266c3735920927d1abbd126609aebeb1573
chenjinghao <chenjinghao@cvte.com>
2024-02-23 20:22:04 +0800

    [config] review telescop keybinding

--- lua/plugin-config/Telescope.lua
+++ lua/plugin-config/Telescope.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "<leader>p", ":Telescope find_files<CR>", opt)
-vim.keymap.set("n", "<leader>P", ":Telescope live_grep<CR>", opt)
+vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", opt)
+vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>", opt)
 vim.keymap.set("n", "<leader>rs", ":Telescope resume<CR>", opt)
 vim.keymap.set("n", "<leader>o", ":Telescope oldfiles<CR>", opt)
 
